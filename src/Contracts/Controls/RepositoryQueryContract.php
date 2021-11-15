2cb9a44fac0257e0f9d439e4de5334aa230883c5
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-15 03:17:07 +0100

    Add methods in RepositoryQueryContract.php

--- src/Contracts/Controls/RepositoryQueryContract.php
+++ src/Contracts/Controls/RepositoryQueryContract.php
@@ -14,6 +14,19 @@ interface RepositoryQueryContract
     public function all($columns = ['*']);
 
     /**
+     * Retrieve the list of data and can add some adjustments to it
+     * Like model's relations...
+     * 
+     * @param string $orderByColumn
+     * @param string $orderBy
+     * @param array $with
+     * @param array $columns
+     * 
+     * @return mixed
+     */
+    public function list($orderByColumn, $orderBy = 'desc', $with = [], $columns = ['*']);
+
+    /**
      * Search data
      * 
      * @param string|int $searchTerm
@@ -21,4 +34,65 @@ interface RepositoryQueryContract
      * @return mixed
      */
     public function search($searchTerm);
+
+    /**
+     * Retrieve first data of repository
+     *
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function first($columns = ['*']);
+
+    /**
+     * Find data by id
+     *
+     * @param       $id
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function find($id, $columns = ['*']);
+
+    /**
+     * Find data by field and value
+     *
+     * @param       $field
+     * @param       $value
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function findByField($field, $value = null, $columns = ['*']);
+
+    /**
+     * Find data by some params
+     * 
+     * @param array $params
+     * @param array $columns
+     * 
+     * @return mixed
+     */
+    public function findBy(array $params, $columns = ['*']);
+
+    /**
+     * Find all data by some params
+     * 
+     * @param array $params
+     * @param array $columns
+     * 
+     * @return mixed
+     */
+    public function findAllBy(array $params, $columns = ['*']);
+
+    /**
+     * Retrieve all data of repository, paginated
+     *
+     * @param null|int $limit
+     * @param array    $columns
+     * @param string   $method
+     *
+     * @return mixed
+     */
+    public function paginate($limit = null, $columns = ['*'], $method = "paginate");
 }
