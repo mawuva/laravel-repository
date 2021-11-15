bf79bbaa11bddb7148326565abd63096b829eed2
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-15 02:47:37 +0100

    Add methods in RepositoryCriteriaContract.php

--- src/Contracts/Criteria/RepositoryCriteriaContract.php
+++ src/Contracts/Criteria/RepositoryCriteriaContract.php
@@ -2,7 +2,55 @@
 
 namespace Mawuekom\Repository\Contracts\Criteria;
 
+use Mawuekom\Repository\Criteria\Criteria;
+
 interface RepositoryCriteriaContract
 {
-    
+    /**
+     * Skip criteria 
+     * 
+     * @param bool $status
+     * 
+     * @return $this
+     */
+    public function skipCriteria($status = true);
+
+    /**
+     * Get criteria
+     * 
+     * @return mixed
+     */
+    public function getCriteria();
+
+    /**
+     * Get by criteria
+     * 
+     * @param \Mawuekom\Repository\Criteria\Criteria $criteria
+     * 
+     * @return $this
+     */
+    public function getByCriteria(Criteria $criteria);
+
+    /**
+     * Push criteria
+     * 
+     * @param \Mawuekom\Repository\Criteria\Criteria $criteria
+     * 
+     * @return $this
+     */
+    public function pushCriteria(Criteria $criteria);
+
+    /**
+     * Apply criteria
+     * 
+     * @return $this
+     */
+    public function applyCriteria();
+
+    /**
+     * Reset criteria
+     * 
+     * @return $this
+     */
+    public function resetCriteria();
 }
