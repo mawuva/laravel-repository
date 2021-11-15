451854c6d28492765df2f78e7c3ffd008ced13c6
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-15 03:06:15 +0100

    Add methods in RepositoryCommandContract.php and RepositoryQueryContract.php

--- src/Contracts/Controls/RepositoryCommandContract.php
+++ src/Contracts/Controls/RepositoryCommandContract.php
@@ -4,5 +4,50 @@ namespace Mawuekom\Repository\Contracts\Controls;
 
 interface RepositoryCommandContract
 {
+    /**
+     * Save a new entity in repository
+     *
+     * @param array $attributes
+     *
+     * @return mixed
+     */
+    public function create(array $attributes);
 
+    /**
+     * Update entity in repository by id
+     *
+     * @param array         $attributes
+     * @param int|string    $id
+     *
+     * @return mixed
+     */
+    public function update(array $attributes, $id);
+
+    /**
+     * Update entity in repository by various params
+     * 
+     * @param array $attributes
+     * @param array $data
+     * 
+     * @return mixed
+     */
+    public function updateBy(array $params, array $attributes);
+
+    /**
+     * Delete data by ID
+     * 
+     * @param int $id
+     * 
+     * @return mixed
+     */
+    public function delete($id);
+
+    /**
+     * Delete data by some params
+     * 
+     * @param array $params
+     * 
+     * @return mixed
+     */
+    public function deleteBy(array $params);
 }
--- src/Contracts/Controls/RepositoryQueryContract.php
+++ src/Contracts/Controls/RepositoryQueryContract.php
@@ -4,5 +4,21 @@ namespace Mawuekom\Repository\Contracts\Controls;
 
 interface RepositoryQueryContract
 {
+    /**
+     * Retrieve all data of repository
+     *
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function all($columns = ['*']);
 
+    /**
+     * Search data
+     * 
+     * @param string|int $searchTerm
+     * 
+     * @return mixed
+     */
+    public function search($searchTerm);
 }
